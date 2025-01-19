e6e2e09d498134d2697f2f71612e9e16a9485f5c
Himanshu rajputt <rajput6600himanshu@gmail.com>
2025-01-19 19:49:08 +0530

    Added 2291  in project

--- Dish_Scanner/src/App.jsx
+++ Dish_Scanner/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// App.jsx
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import AllDishes from "./Components/AllDishes";
+import AddDish from "./Components/AddDish";
+import EditDish from "./Components/EditDish";
+// import Home from "./Components/Scanner";
+import NotFound from "./Components/notFound";
+import Scanner from "./Components/Scanner";
+import Login from "./Components/Login";
+import SignUp from "./Components/Signup";
+import Home from "./Components/Home";
+import AdminLogin from "./Components/Admin/AdminLogin";
+import { NotAdmin } from "./Components/Route/NotAdmin";
+import ProtectedRoutes from "./Components/Route/ProtectedRoutes";
+import Calorie from "./Components/Calorie";
+import Team from "./Components/Team";
+import About from "./Components/About";
+import KnowWhy from "./Components/KnowWhy";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calorie" element={<Calorie />} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/KnowWhy" element={<KnowWhy />} />
+
+        <Route path="/Scanner" element={<Scanner />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Signup" element={<SignUp />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/all-dishes" element={<AllDishes />} />
+          <Route path="/add-dish" element={<AddDish />} />
+          <Route path="/edit/:id" element={<EditDish />} />
+        </Route>
+
+        <Route path="/notAdmin" element={<NotAdmin />} />
+        <Route path="/AdminLogin" element={<AdminLogin />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
